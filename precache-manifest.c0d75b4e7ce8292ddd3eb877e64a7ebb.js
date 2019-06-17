601d4d50bbd95fe54a91cb641850f44d3c089407
Anthony Gringeri <antgringeri@gmail.com>
2019-06-17 03:48:18 -0400

    Updates

---
+++ precache-manifest.c0d75b4e7ce8292ddd3eb877e64a7ebb.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4412bce616f1bca48600f570979bd6f7",
+    "url": "/personal-site/index.html"
+  },
+  {
+    "revision": "823afd7d09e101abe803",
+    "url": "/personal-site/static/css/main.0553b72e.chunk.css"
+  },
+  {
+    "revision": "37137776fe136dbfed17",
+    "url": "/personal-site/static/js/2.722c0179.chunk.js"
+  },
+  {
+    "revision": "823afd7d09e101abe803",
+    "url": "/personal-site/static/js/main.eec8dfc9.chunk.js"
+  },
+  {
+    "revision": "d33cc63b615d83e32684",
+    "url": "/personal-site/static/js/runtime~main.039145d2.js"
+  },
+  {
+    "revision": "5f25aefb4c650272902048817bd25349",
+    "url": "/personal-site/static/media/data_uncorked_screenshot.5f25aefb.png"
+  },
+  {
+    "revision": "e1a63d00f6197219776502faf85c4294",
+    "url": "/personal-site/static/media/experiment_results.e1a63d00.png"
+  },
+  {
+    "revision": "91749a89403cd6601f40f94d2bceda8e",
+    "url": "/personal-site/static/media/first_year_hub_screenshot.91749a89.png"
+  },
+  {
+    "revision": "d09a84daec2ebed8203caf4f051a957b",
+    "url": "/personal-site/static/media/fundacion_logo.d09a84da.png"
+  },
+  {
+    "revision": "68c0500d171036525e13e5ffab48e1ab",
+    "url": "/personal-site/static/media/graphical_experiment_screenshot.68c0500d.png"
+  },
+  {
+    "revision": "a640f74c8158ccecdb50ab5b4c25d379",
+    "url": "/personal-site/static/media/hhs_logo.a640f74c.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal-site/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e2a579bba75997734f5055b4e224c3d5",
+    "url": "/personal-site/static/media/me_1.e2a579bb.jpg"
+  },
+  {
+    "revision": "e39dfb934c57e5b81154f443dd2d146d",
+    "url": "/personal-site/static/media/resume.e39dfb93.pdf"
+  },
+  {
+    "revision": "632afeb281b450c571833e1cf33a9c51",
+    "url": "/personal-site/static/media/wpi_logo.632afeb2.png"
+  }
+]);
